b78567c90115ebac968e90d15e9c4eac98668e30
TorreJohnson <torre.johnson@gmail.com>
2018-02-19 15:51:32 -0500

    completed Map Lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file.
+function lowerCaseDrivers(arr) {
+  return arr.map(function(user) {return user.toLowerCase()});
+}
+
+function nameToAttributes(arr) {
+  let newArr = []
+  arr.map(function(user) {
+    newArr.push({firstName: user.split(" ")[0], lastName: user.split(" ")[1]});
+  })
+  return newArr;
+}
+
+function attributesToPhrase(arr) {
+  return arr.map(function(user) {return `${user.name} is from ${user.hometown}`});
+}
